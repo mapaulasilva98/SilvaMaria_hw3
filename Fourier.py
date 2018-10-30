c2ce7eb9835e49ed436b80c047a923747165a8c4
Maria Paula <mariapaula@MacBook-Pro-de-Maria-2.local>
2018-10-30 08:42:56 -0500

    lista la primera grafica de signal

--- Fourier.py
+++ Fourier.py
@@ -5,13 +5,28 @@ import math
 
 # Almacene los datos de signal.dat y de incompletos.dat
 
-sigA= signal.dat[0]
+datosSignal=np.genfromtxt("signal.dat", delimiter=",")
 
-sigB= signal.dat[1]
-
-
-
-print(sigA)
+datosIncompletos=np.genfromtxt("incompletos.dat", delimiter=",")
+ 
+# Haga una grafica de los datos de signal.dat y guarde dicha grafica sin mostrarla en ApellidoNombre_signal.pdf.
 
 
+sigx=datosSignal[:,0]
+sigy=datosSignal[:,1]
 
+
+plt.figure()
+plt.plot(sigx,sigy)
+plt.xlabel("Tiempo")
+plt.ylabel("y(t)")
+plt.title("signal")
+plt.savefig("SilvaMaria_signal.pdf")
+
+
+#Haga la transformada de Fourier de los datos de la senal usando su implementacio propia de la transformada discreta de fourier.
+
+# mi numero de puntos 
+n= len(sigx)
+
+dt=sigx[1]-sigx[0]
