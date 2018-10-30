5c39515e2931acaceb63401f0b078a1585be8fc0
Maria Paula <mariapaula@MacBook-Pro-de-Maria-2.local>
2018-10-30 10:17:12 -0500

    ya esta listo el filtro y la transformada inversa

--- Fourier.py
+++ Fourier.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import math 
+from scipy import fftpack
+from scipy.fftpack import fft,fftfreq,ifft
+
 
 
 # Almacene los datos de signal.dat y de incompletos.dat
@@ -29,4 +32,75 @@ plt.savefig("SilvaMaria_signal.pdf")
 # mi numero de puntos 
 n= len(sigx)
 
+# mi delta de tiempo 
 dt=sigx[1]-sigx[0]
+
+# mi frecuencia 
+frec= fftfreq(n,dt)
+
+# suma para mi transformada de fourier 
+
+suma= np.linspace (0,0,n)
+
+
+for i in range (n):
+	for j in range (len(suma)):
+		suma[i] = suma[i] + (sigy[j]*(np.e**((-1j)*2*np.pi*j*i/n)))
+
+
+trans= abs(suma)/n
+
+trans2= (suma)/n
+
+#Haga una grafica de la transformada de Fourier y guarde dicha grafica sin mostrarla en ApellidoNombre_TF.pdf.
+
+plt.figure()
+plt.plot(frec, trans )
+plt.title("Transformada de Fourier")
+plt.xlabel("frecuencia")
+plt.ylabel("transformada")
+#plt.show()
+plt.savefig("SilvaMaria_TF.pdf")
+
+
+#Imprima un mensaje donde indique cuales son las frecuencias principales de su senal.
+
+principales=[]
+
+for i in range (len (frec)):
+	if (trans[i]>0.5):
+		principales.append(frec[i])
+
+
+print ("Las frecuencias principales de mi senal son:", principales)
+
+
+#Haga un filtro pasa bajos con frecuencia de corte fc igual a 1000 Hz.
+#realice la transformada inversa y haga una grafica de la senal filtrada. 
+#Guarde dicha grafica sin mostrarla en ApellidoNombre_filtrada.pdf.
+
+
+
+for i in range (len (trans2)):
+	if (abs(frec[i])<=1000):
+		trans2[i]=trans2[i]
+	else: 	
+		trans2[i]=0
+
+inversa=ifft(trans2)
+
+
+plt.figure()
+plt.title("Transformada filtrada ")
+plt.xlabel("frecuencia")
+plt.ylabel("Transformada")
+plt.plot(sigx,inversa)
+#plt.show()
+plt.savefig("SilvaMaria_filtrada.pdf")
+
+
+
+
+
+
+
